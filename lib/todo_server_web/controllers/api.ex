ad971a52396b0dee3f4a5c61f0ac9aff74f7a837
Kozlov Egor Alexandrovich <PublicDevelMail@yandex.ru>
2026-03-28 14:00:54 +0300

    Controller (/lib/controllers/api.ex) is refactored bit.

--- lib/todo_server_web/controllers/api.ex
+++ lib/todo_server_web/controllers/api.ex
@@ -1,35 +1,49 @@
 defmodule TodoServerWeb.API.Controller do
   use TodoServerWeb, :controller
 
-  defmacro _items_match(items) do
-    is_list(items)
+  def _construct_response(
+    conn, items, action = %{ "type" => _, "argument" => _}) do
+    conn
+    |> put_resp_content_type("application/json")
+    |> put_status(:ok)
+    |> json(%{
+      "status" => :ok, "items" => items, "action" => action})
   end
 
-  def edit(conn, %{"items" => items, "target" => target}) when is_list(items) do
-     if target in items do
-       conn
-       |> put_resp_content_type("application/json")
-       |> put_status(:conflict) # Conflict
-       |> json(%{ "status" => :conflict, "items" => items, "action" => nil})
-      else
-        conn
-        |> put_resp_content_type("application/json")
-        |> put_status(:ok)
-        |> json(%{ "status" => :ok, "items" => items, "action" => %{ "type" => "add", "argument" => target }})
-    end
+  def _construct_response(conn, items, nil) do
+    conn
+    |> put_resp_content_type("application/json")
+    |> put_status(:conflict)
+    |> json(
+      %{
+        "status" => :conflict,
+        "items" => items, "action" => nil})
   end
 
-  def delete(conn, %{"items" => items, "target" => target}) when is_list(items) do
-    if target in items do
-      conn
-      |> put_resp_content_type("application/json")
-      |> put_status(:ok)
-      |> json(%{ "status" => :ok, "items" => items, "action" => %{ "type" => "remove", "argument" => Enum.find_index(items, fn x -> x == target end) }})
-    else
-      conn
-      |> put_resp_content_type("application/json")
-      |> put_status(:conflict) # Conflict
-      |> json(%{ "status" => :conflict, "items" => items, "action" => nil})
-    end
+  def _construct_response_selected(conn, item, action, true) do
+    _construct_response(conn, item, action)
+  end
+
+  def _construct_response_selected(conn, item, _, false) do
+    _construct_response(conn, item, nil)
+  end
+
+  def edit(conn, %{"items" => items, "target" => target}) do
+    _construct_response_selected(
+      conn,
+      items,
+      %{ "type" => "add", "argument" => target },
+      target not in items
+    )
+  end
+
+  def delete(conn, %{"items" => items, "target" => target}) do
+    index = Enum.find_index(items, fn x -> x == target end)
+    _construct_response_selected(
+      conn,
+      items,
+      %{ "type" => "remove", "argument" => index },
+      !is_nil(index)
+    )
   end
 end
