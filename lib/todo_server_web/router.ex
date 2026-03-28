7e5c7903ddce954901364175f6c2d64c3c0066ad
Kozlov Egor Alexandrovich <PublicDevelMail@yandex.ru>
2026-03-28 12:45:28 +0300

    Removed html files.

--- lib/todo_server_web/router.ex
+++ lib/todo_server_web/router.ex
@@ -1,21 +1,8 @@
 defmodule TodoServerWeb.Router do
   use TodoServerWeb, :router
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
 
   pipeline :api do
     plug :accepts, ["json"]
-  end
-
-  scope "/", TodoServerWeb do
-    pipe_through :browser
-
-    get "/", PageController, :index
   end
 
   # Other scopes may use custom stacks.
@@ -37,7 +24,6 @@ defmodule TodoServerWeb.Router do
     import Phoenix.LiveDashboard.Router
 
     scope "/" do
-      pipe_through :browser
       live_dashboard "/dashboard", metrics: TodoServerWeb.Telemetry
     end
   end
